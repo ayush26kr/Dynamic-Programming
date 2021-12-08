e9f73e065058495da57b4c9293a811d968e4c8d4
AYUSH KUMAR <57504080+ayush26kr@users.noreply.github.com>
2021-12-08 22:19:51 +0530

    Create recursive knapsack.cpp

---
+++ recursive knapsack.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int knapSack(int W,int wt[],int val[],int n){
+    
+    if(n==0||W==0){
+        return 0;
+    }
+    if(wt[n-1]<=W) 
+    return max(val[n-1]+knapSack(W-wt[n-1],wt,val,n-1),knapSack(W,wt,val,n-1));
+    else
+    return knapSack(W,wt,val,n-1);
+}
+int main()
+{
+    int val[] = { 60, 100, 120 };
+    int wt[] = { 10, 20, 30 };
+    int W = 50;                //capacity
+    int n = sizeof(val) / sizeof(val[0]);  //number of items
+    cout << "Maximum profit is: "<<knapSack(W, wt, val, n);
+    return 0;   
+}
