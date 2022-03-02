ac8afee4340e142cb9d6746d221cbd417b6c8ee0
AYUSH KUMAR <57504080+ayush26kr@users.noreply.github.com>
2022-03-02 23:23:43 +0530

    Tabulation

---
+++ nth Fibonacci number (Tabulation).cpp
@@ -0,0 +1,19 @@
+//Using Tabulation
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter value to be find (n) :";
+    cin>>n;
+    int prev2=0;
+    int prev1=1;
+    int curr;
+    for(int i=2;i<=n;i++){
+        curr=prev1+prev2;
+        prev2=prev1;
+        prev1=curr;
+    }
+    cout<<prev1;
+}
